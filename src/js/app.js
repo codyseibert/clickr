32cc8b98487ec5d579b6b5265eda4bda0e319a48
nangtrongvuon <morepositive@outlook.com>
2015-10-05 21:04:26 +0700

    navigation finished, game loop WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,59 +1,82 @@
 $(document).ready(function() {
   // This callback function is invoked when the page is done loading
 
+// Features
+
+navigationClick();
+menuBarClick();
+
+
+})
+
+function menuBarClick() {
+
+  $($("a").not("#startButton")).click(function() {
+
+  $("#practice, #global, #stats, #about").hide();
+  $($(this).attr("href")).show();
+
+});
+
+}
+
+function navigationClick() {
+
+  $("#title").click(function() {
+
+     $("#navigationBar").slideToggle();
+     $("#title").text( "Clickr Menu" );
+
+   });
+
+}
+
+ function gameStart() {
+
+  $("#startButton").click(function() {
+
+
   // Declaring score and box count
 
   var score = 0;
-    var boxCount = 0;
-
-  // What do we need to do?
-  alert("Click the box to delete it! You lose if there are more than 10 boxes on the page!");
-  // Task 1: Spawn random boxes on the page every second
-
-  // Well, first off, let's make a interval for every second
-  // I'm making this the game loop - Nắng
+  var boxCount = 0;
   var ONE_SECOND = 1000; // 1000ms === 1s
 
-  gameLoop = setInterval(function(){
-    // This callback is invoked every second
-    // unless the player loses.
+  // I'm making this the game loop - Nắng
 
-    // We need to spawn a box here
+  gameLoop = setInterval(function(){
+
+
     var $box = createBox();
-    appendBoxToBody($box);
+    appendBoxToPlayfield($box);
     positionBoxRandomly($box);
 
 
-    // How do we find a random position?
   }, ONE_SECOND);
 
   function Position(left, top) {
+
     this.left = left;
     this.top = top;
 
   }
 
   function createBox() {
-    // TODO: Create a DIV using jquery
-    // TODO: add the class "box" to the DIV
-    // TODO: bind an event listener to the 'click' event
-    //    HINT: Use "boxClicked" method
 
     var $div = $("<div>", {class: "box"});
     boxCount++;
     $("#boxCount").text("Box count: " + boxCount);
     $div.click(function() {
+
       $( this ).remove();
       score++;
       boxCount--;
       $("#score").text("Score: " + score);
       $("#boxCount").text("Box count: " + boxCount);
 
-
-
     });
     // Player's lose condition
-    if (boxCount == 11) {
+    if (boxCount >= 11) {
         clearInterval(gameLoop);
         alert("You lose!")
     }
@@ -62,15 +85,11 @@ $(document).ready(function() {
 
   }
 
-
-
-  function appendBoxToBody($box) {
-    // TODO: append the $box argument passed in to the body of the page
-    $("body").append($box);
+  function appendBoxToPlayfield($box) {
+    $(".playfield").append($box);
   }
 
   function positionBoxRandomly($box) {
-    // TODO: set the top and left attributes of the "$box" style to be somewhere
     // random on the page.
     // Hint: use the "getRandomPosition" function to get a position object
 
@@ -79,22 +98,25 @@ $(document).ready(function() {
   }
 
   function getRandomPosition() {
+
     return new Position(getRandomLeft(), getRandomTop());
+
   }
 
   function getRandomLeft() {
-    // TODO: Return a random position between 0 and the width of the window
 
-      return Math.floor(Math.random() * ($( window ).width() - 100));
+      return Math.floor(Math.random() * ($( ".playfield" ).width() - 100));
       // 100 is to prevent boxes spawning out of bounds, still needs improvement
 
     }
 
   function getRandomTop() {
-    // TODO: Return a random position between 0 and the width of the window
 
-      return Math.floor(Math.random() * ($( window ).height() - 100));
+      return Math.floor(Math.random() * ($( ".playfield" ).height() - 100));
       // 100 is to prevent boxes spawning out of bounds, still needs improvement
 
-  }
-});
+    }
+  })
+
+
+;}
