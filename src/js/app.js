9893d182ea0473a1284d828bd863ecb8b291c797
nangtrongvuon <morepositive@outlook.com>
2015-10-07 17:51:50 +0700

    finished the next step
    the game is working as expected!

--- src/js/app.js
+++ src/js/app.js
@@ -5,16 +5,21 @@ $(document).ready(function() {
 
 navigationClick();
 menuBarClick();
+gameStart();
 
 
 })
+
+
 
 function menuBarClick() {
 
   $($("a").not("#startButton")).click(function() {
 
   $("#practice, #global, #stats, #about").hide();
-  $($(this).attr("href")).show();
+
+  $($(this).attr("href")).show("slow", function(e) {
+  });
 
 });
 
@@ -33,26 +38,55 @@ function navigationClick() {
 
  function gameStart() {
 
+
+
+
+
   $("#startButton").click(function() {
+
+  accuracyCount();
+  $("#practice").append(
+    $(".score"),
+    $(".boxCount"),
+    $(".accuracyCount"),
+    $('.timer')
+
+  )
 
 
   // Declaring score and box count
-
+  $("#startButton").hide(100);
+  $("#practice > .box").remove();
   var score = 0;
   var boxCount = 0;
+  var clickCount = 0;
+  var timeElapsed = new Date;
   var ONE_SECOND = 1000; // 1000ms === 1s
 
   // I'm making this the game loop - Nắng
 
   gameLoop = setInterval(function(){
 
-
-    var $box = createBox();
+    $('.timer').text("Time elasped:" + Math.round((new Date - timeElapsed) / 1000, 0) + " Seconds");
+    var $box = mainGameCount();
     appendBoxToPlayfield($box);
     positionBoxRandomly($box);
+    checkForVisible();
 
 
   }, ONE_SECOND);
+
+  function accuracyCount() {
+
+       $($("#practice").not("#startButton")).click(function() {
+
+         clickCount++;
+
+       });
+
+     }
+
+
 
   function Position(left, top) {
 
@@ -61,32 +95,63 @@ function navigationClick() {
 
   }
 
-  function createBox() {
+  function checkForVisible(){
+    if ($("#practice").is(":hidden")){
+      clearInterval(gameLoop);
+      $(".box").remove();
+      $("#startButton").show(100);
+
+  }
+
+}
+
+
+  function mainGameCount() {
 
     var $div = $("<div>", {class: "box"});
     boxCount++;
-    $("#boxCount").text("Box count: " + boxCount);
+    $(".boxCount").text("Box count: " + boxCount);
     $div.click(function() {
 
       $( this ).remove();
       score++;
       boxCount--;
-      $("#score").text("Score: " + score);
-      $("#boxCount").text("Box count: " + boxCount);
+      $(".score").text("Score: " + score);
+      $(".boxCount").text("Box count: " + boxCount);
+      $(".accuracyCount").text("Accuracy: " + Math.round((score / clickCount) * 100, 2) + "%");
+
 
     });
+
     // Player's lose condition
     if (boxCount >= 11) {
         clearInterval(gameLoop);
-        alert("You lose!")
+        alert("You lose!");
+        $("#practice").hide();
+        $("#stats").show();
+        $(".boxCount").remove();
+        $("#startButton").show(100);
+        $("#practice > div").unbind("click");
+        $("#stats").append(
+          $(".score").text("Score: " + score),
+          $(".accuracyCount").text("Accuracy: " + Math.round((score / (clickCount - 1)) * 100, 2) + "%"),
+          $('.timer').text("Time elasped:" + Math.round((new Date - timeElapsed) / 1000, 0) + " Seconds")
+
+        );
     }
 
     return $div;
 
   }
 
+  function checkForVisibility() {
+    if ( $("#practice").css('display') == 'none' ){
+      clearInterval(gameLoop);
+}
+  }
+
   function appendBoxToPlayfield($box) {
-    $(".playfield").append($box);
+    $("#practice").append($box);
   }
 
   function positionBoxRandomly($box) {
@@ -105,18 +170,15 @@ function navigationClick() {
 
   function getRandomLeft() {
 
-      return Math.floor(Math.random() * ($( ".playfield" ).width() - 100));
-      // 100 is to prevent boxes spawning out of bounds, still needs improvement
+      return Math.floor((Math.random() * $( "#practice" ).width()) + 0);
 
     }
 
   function getRandomTop() {
 
-      return Math.floor(Math.random() * ($( ".playfield" ).height() - 100));
-      // 100 is to prevent boxes spawning out of bounds, still needs improvement
+      return Math.floor((Math.random() * $( "#practice" ).height()) + 0);
 
     }
-  })
 
 
-;}
+})}
